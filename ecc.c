89573b21c443c2ca4bfbf921bb7c1ae5225e5187
Manuel Osterno | Adahil Muniz <adahilmuniz@hotmail.com>
2024-06-02 15:01:07 -0300

    Update ecc

--- ecc.c
+++ ecc.c
@@ -1,5 +1,35 @@
 #include "ecc.h"
 
+//@NOTE: For simple hamming code, considering n == word bits number and r == refundancy bits number.
+//2^r - 1 >= n, so, for n==64, r==7 and n==32, r==6. For extended hamming code, add one to r result.
+
+//@NOTE: For code efficiency, we will create two function, one for (73, 64) and another one for (39, 32).
+//However, it is always possible to implement a method to search for a r value given an n value.
+
+
+void ham64encode(uint32_t * data_in, uint32_t * data_out){
+    uint8_t ecc = 0;
+
+    /*
+    ecc = GET_D(1 , data_in[0]) ^ \
+          GET_D(2 , data_in[0]) ^ \
+          GET_D(4 , data_in[0]) ^ \
+          GET_D(5 , data_in[0]) ^ \
+          GET_D(7 , data_in[0]) ^ \
+          GET_D(9 , data_in[0]) ^ \
+          GET_D(11, data_in[0]) ^ \
+          GET_D(12, data_in[0]) ^ \
+          GET_D(14, data_in[0]) ^ \
+          GET_D(14, data_in[0]) ^ \
+          GET_D(14, data_in[0]) ^ \
+          GET_D(14, data_in[0]) ^ \
+
+      for (uint32_t i = 1; i <= 73; i++){
+        
+      }
+    */
+}
+
 void encode(uint32_t * data_in, uint32_t * data_out) {
 
 }
--- ecc.h
+++ ecc.h
@@ -3,10 +3,27 @@
 
     #include <stdint.h>
 
+    //Mask difines consider 32 bit data
+    #define MASK_D1 0b10000000000000000000000000000000
+    #define MASK_D(n) (MASK_D1 >> (n-1))
+    #define GET_D(n, data) ((data & MASK_D(n)) >> (32-n))
+
     typedef struct {
         uint16_t nb_non_corrected;
         uint16_t nb_corrected;
     } error_t;
+
+    /*
+    typedef union {
+        uint32_t data[2];
+        uint8_t ecc;
+    } hamm64_t;
+
+    typedef union {
+        uint32_t data;
+        uint8_t ecc;
+    } hamm32_t;
+    */
 
     void    encode(uint32_t * data_in, uint32_t * data_out);
     error_t decode(uint32_t * data_in, uint32_t * data_out);
