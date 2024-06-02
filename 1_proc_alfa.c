94c7940968337b149613b2c4f22dc408c016dbe0
Manuel Osterno | Adahil Muniz <adahilmuniz@hotmail.com>
2024-06-02 15:04:05 -0300

    Process Alfa waiting for file content

--- 1_proc_alfa.c
+++ 1_proc_alfa.c
@@ -18,17 +18,20 @@ int main (){
     }
 
     printf("[PROC ALFA] Starting Process Alfa ...\n");
-    connect(&con, CHANNEL_0, "ab+"); //Channel 0 is the communication channel between Alfa an Error Injector
-    while(fgets(file_content, PAYLOAD_SIZE*4, fptr)) {
+    while(1) {
+        //@NOTE: The file is not written until it is closed, so, we need to open and close connection inside this loop
+        connect(&con, CHANNEL_0, "ab+"); //Channel 0 is the communication channel between Alfa an Error Injector
+        printf("[PROC ALFA] Waiting for reading file ...\n");
+        while(!fgets(file_content, PAYLOAD_SIZE*4, fptr));
         encode_packet((uint32_t * ) file_content, &packet, PAYLOAD_SIZE, 2024, pkt_nb);
         print_packet(packet, PAYLOAD_SIZE);
         send_pckt(packet, &con);
         pkt_nb ++;
+        close_connect(&con);
     }
     printf("\n");
 
     fclose(fptr);
-    close_connect(&con);
 
     printf("[PROC ALFA] Finishing Process Alfa ...\n");
     return 0;
